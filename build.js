073d2c0675c64e8be78dff5bbd2320ac27212e19
w3slyn <weslyn4033@gmail.com>
2024-11-11 17:42:46 -0300

    new commit

---
+++ build.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const ejs = require('ejs');
+
+// Defina os caminhos
+const templatesDir = path.join(__dirname, 'src', 'views');
+const buildDir = path.join(__dirname, 'dist');
+
+// Crie a pasta de build se não existir
+if (!fs.existsSync(buildDir)){
+    fs.mkdirSync(buildDir);
+}
+
+// Processa os arquivos EJS
+fs.readdir(templatesDir, (err, files) => {
+    if (err) throw err;
+    files.forEach(file => {
+        if (path.extname(file) === '.ejs') {
+            const templatePath = path.join(templatesDir, file);
+            const buildPath = path.join(buildDir, path.basename(file, '.ejs') + '.html');
+
+            ejs.renderFile(templatePath, {}, (err, str) => {
+                if (err) throw err;
+                fs.writeFileSync(buildPath, str);
+                console.log(`Built ${buildPath}`);
+            });
+        }
+    });
+});
